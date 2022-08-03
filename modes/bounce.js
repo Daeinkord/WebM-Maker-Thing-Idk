33cbee906a2242dcb2443ff9ee33ffc0173e237b
Daeinkord <48444373+Daeinkord@users.noreply.github.com>
2022-08-03 23:44:31 +0300

    Delete bounce.js

--- modes/bounce.js
+++
@@ -1,7 +0,0 @@
-const util = require('../util.js')
-module.exports = {
-	setup: () => {},
-	getFrameBounds: (info) => ({
-		height: info.frame === 0 ? info.maxHeight : Math.floor(Math.abs(Math.cos((info.frame / (info.frameRate / info.tempo)) * Math.PI) * (info.maxHeight - util.delta))) + util.delta,
-	}),
-}
